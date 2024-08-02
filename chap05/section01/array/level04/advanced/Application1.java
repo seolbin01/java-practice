36c55ff05b7efcef553f8fa07f2ce9e5c0cb56f0
seolbin01 <se01bin@suwon.ac.kr>
2024-08-02 17:17:29 +0900

    fix: 일부 코드 수정

--- chap05/section01/array/level04/advanced/Application1.java
+++ chap05/section01/array/level04/advanced/Application1.java
@@ -6,13 +6,28 @@ public class Application1 {
     public static void main(String[] args) {
         int[] arr = new int[6];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random() * 45) + 1;
+        int num = 0;
+        loop:
+        while (num < 6) { // 0~5 까지 인덱스로 들어감
+            arr[num] = (int) (Math.random() * 45) + 1;
+
+            if (num != 0) { //1~5
+                for (int i = 0; i < num; i++) {
+                    if (arr[i] == arr[num]) {
+                        continue loop;
+                    }
+                }
+            }
+
+            num++;
         }
+
 
         Arrays.sort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
+        for (
+                int i = 0;
+                i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
